37c3a5fccf6d6b00fa3d8ffd3bbd2cdb321a1b27
kimkohlenbach <kim.kohlenbach@digitaldesk.com.br>
2018-04-27 15:28:59 -0300

    Criação do projeto com o banco de dados e página de cadastro de usuários

---
+++ DiScarpe/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using DiScarpe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiScarpe.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Usuario obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                DiScarpeDBEntities db = new DiScarpeDBEntities();
+                db.Usuario.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+    }
+}
