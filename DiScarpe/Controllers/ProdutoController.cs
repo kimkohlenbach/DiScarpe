ae0bb8a968787874ad2800f6b582830710d3c5cb
kimkohlenbach <kim.kohlenbach@digitaldesk.com.br>
2018-04-30 09:39:41 -0300

    Apresentação de dado do banco na view

---
+++ DiScarpe/Controllers/ProdutoController.cs
@@ -0,0 +1,36 @@
+﻿using DiScarpe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiScarpe.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private DiScarpeDBEntities db = new DiScarpeDBEntities();
+
+
+        public ActionResult Index()
+        {
+            var Produto = db.Produto;
+            return View(Produto);
+        }
+
+        [HttpPost]
+        public ActionResult Index(DiScarpe.Models.Produto produtos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produto.Add(produtos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produtos);
+        }
+
+
+
+    }
+}
