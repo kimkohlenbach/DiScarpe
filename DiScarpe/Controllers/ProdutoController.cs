0830227aa0ca1c8dbb83da871d4324a1cb303844
kimkohlenbach <kim.kohlenbach@digitaldesk.com.br>
2018-05-03 15:30:03 -0300

    Reorganização dos controllers e views

--- DiScarpe/Controllers/ProdutoController.cs
+++ DiScarpe/Controllers/ProdutoController.cs
@@ -56,5 +56,81 @@ namespace DiScarpe.Controllers
             return View(db.Produto.FirstOrDefault(x => x.IdProduto == produtoId));
         }
 
+        // GET: CadastroProduto
+        public ActionResult Adicionar()
+        {
+            {
+                DiScarpeDBEntities db = new DiScarpeDBEntities();
+                var Categoria = db.Categoria;
+                List<SelectListItem> cat = new List<SelectListItem>();
+                foreach (var i in Categoria)
+                {
+                    cat.Add(new SelectListItem
+                    {
+                        Text = i.Descricao,
+                        Value = i.IdCategoria.ToString()
+                    });
+                }
+                ViewBag.cat = cat;
+
+                var Color = db.Cor;
+                List<SelectListItem> color = new List<SelectListItem>();
+                foreach (var i in Color)
+                {
+                    color.Add(new SelectListItem
+                    {
+                        Text = i.Descricao,
+                        Value = i.IdCor.ToString()
+                    });
+                }
+                ViewBag.color = color;
+
+                var marca = db.Marca;
+                List<SelectListItem> listaMarca = new List<SelectListItem>();
+                foreach (var i in marca)
+                {
+                    listaMarca.Add(new SelectListItem
+                    {
+                        Text = i.Descricao,
+                        Value = i.IdMarca + ""
+                    });
+                }
+                ViewBag.marca = listaMarca;
+
+                var tamanho = db.Tamanho;
+                List<SelectListItem> listaTamanhos = new List<SelectListItem>();
+                foreach (var i in tamanho)
+                {
+                    listaTamanhos.Add(new SelectListItem
+                    {
+                        Text = i.Descricao + "",
+                        Value = i.IdTamanho + ""
+                    });
+                }
+                ViewBag.tamanho = listaTamanhos;
+
+                var estilo = db.Estilo;
+                List<SelectListItem> listaEstilos = new List<SelectListItem>();
+                foreach (var i in estilo)
+                {
+                    listaEstilos.Add(new SelectListItem
+                    {
+                        Text = i.Descricao,
+                        Value = i.IdEstilo + ""
+                    });
+                }
+                ViewBag.estilo = listaEstilos;
+                return View();
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Adicionar(Produto P)
+        {
+            DiScarpeDBEntities db = new DiScarpeDBEntities();
+            db.Produto.Add(P);
+            db.SaveChanges();
+            return Content("alert('Inserido com  sucesso')");
+        }
     }
 }
