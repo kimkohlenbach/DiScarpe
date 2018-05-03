04d87d83ffce04dd035538e7b4cbcd8440d35101
Rodrigo Kirschner Ben <rodrigo.ben@digitaldesk.com.br>
2018-05-03 09:59:08 -0300

    Adicionando a classe CadastroProdutoController.cs  para começar a parte de cadastro dos calçados

---
+++ DiScarpe/Controllers/CadastroProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiScarpe.Controllers
+{
+    public class CadastroProdutoController : Controller
+    {
+        // GET: CadastroProduto
+        public ActionResult Index()
+        {
+            return Content("Teste");
+        }
+    }
+}
