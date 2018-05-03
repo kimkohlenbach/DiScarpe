86cd3822a38546ef0ad28cdc9824ec041f1257b4
Rodrigo Kirschner Ben <rodrigo.ben@digitaldesk.com.br>
2018-05-03 10:34:47 -0300

    Feita a tela básica de CadastroProduto/AddMercadoria.cshtml e junto acrescentado o Action Result no controller responsável pela adição do mesmo no banco de dados

--- DiScarpe/Controllers/CadastroProdutoController.cs
+++ DiScarpe/Controllers/CadastroProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DiScarpe.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,80 @@ namespace DiScarpe.Controllers
     public class CadastroProdutoController : Controller
     {
         // GET: CadastroProduto
-        public ActionResult Index()
+        public ActionResult AddMercadoria()
         {
-            return Content("Teste");
+            {
+                DiScarpeDBEntities db = new DiScarpeDBEntities();
+                var Categoria = db.Categoria;
+                List<SelectListItem> cat = new List<SelectListItem>();
+                foreach (var i in Categoria)
+                {
+                    cat.Add(new SelectListItem
+                    {
+                        Text = i.Descricao,
+                        Value = i.IdCategoria.ToString()
+                    });
+                }
+                ViewBag.cat = cat;
+
+                var Color = db.Cor;
+                List<SelectListItem> color = new List<SelectListItem>();
+                foreach (var i in Color)
+                {
+                    color.Add(new SelectListItem
+                    {
+                        Text = i.Descricao,
+                        Value = i.IdCor.ToString()
+                    });
+                }
+                ViewBag.color = color;
+
+                var marca = db.Marca;
+                List<SelectListItem> listaMarca = new List<SelectListItem>();
+                foreach (var i in marca)
+                {
+                    listaMarca.Add(new SelectListItem
+                    {
+                        Text = i.Descricao,
+                        Value = i.IdMarca + ""
+                    });
+                }
+                ViewBag.marca = listaMarca;
+
+                var tamanho = db.Tamanho;
+                List<SelectListItem> listaTamanhos = new List<SelectListItem>();
+                foreach (var i in tamanho)
+                {
+                    listaTamanhos.Add(new SelectListItem
+                    {
+                        Text = i.Descricao + "",
+                        Value = i.IdTamanho + ""
+                    });
+                }
+                ViewBag.tamanho = listaTamanhos;
+
+                var estilo = db.Estilo;
+                List<SelectListItem> listaEstilos = new List<SelectListItem>();
+                foreach (var i in estilo)
+                {
+                    listaEstilos.Add(new SelectListItem
+                    {
+                        Text = i.Descricao,
+                        Value = i.IdEstilo + ""
+                    });
+                }
+                ViewBag.estilo = listaEstilos;
+                return View();
+            }
+
+        }
+        [HttpPost]
+        public ActionResult AddMercadoria(Produto P)
+        {
+            DiScarpeDBEntities db = new DiScarpeDBEntities();
+            db.Produto.Add(P);
+            db.SaveChanges();
+            return Content("alert('Inserido com  sucesso')");
         }
     }
 }
