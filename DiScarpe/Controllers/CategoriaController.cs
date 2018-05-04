e86e6c07698648bef1cdbf147092ccc9dcc72f36
Rodrigo Kirschner Ben <rodrigo.ben@digitaldesk.com.br>
2018-05-04 16:56:09 -0300

    Adicionados links para adicionar atributos do produto direto na tela de cadastro dele para evitar muita transição vai ser usado modais

---
+++ DiScarpe/Controllers/CategoriaController.cs
@@ -0,0 +1,25 @@
+﻿using DiScarpe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiScarpe.Controllers
+{
+    public class CategoriaController : Controller
+    {
+       public ActionResult Adicionar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Adicionar(Categoria C)
+        {
+            DiScarpeDBEntities db = new DiScarpeDBEntities();
+            db.Categoria.Add(C);
+            db.SaveChanges();
+            return RedirectToAction("Adicionar", "Produto");
+        }
+    }
+}
