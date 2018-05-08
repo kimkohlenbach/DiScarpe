c1ceacc70ff8cc62751d952d8915726bd0c5da79
kimkohlenbach <kim.kohlenbach@digitaldesk.com.br>
2018-05-08 16:37:36 -0300

    Design - barra de navegação

--- DiScarpe/Scripts/script.js
+++ DiScarpe/Scripts/script.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
     $('.nTamanho').click(function () {
-        $('.nTamanho.active').removeClass("active"); //aqui removemos a class do item anteriormente clicado para que possamos adicionar ao item clicado
-        $(this).addClass("active"); //aqui adicionamos a class ao item clicado
+        $('.nTamanho.active').removeClass("active");
+        $(this).addClass("active");
     });
 });
