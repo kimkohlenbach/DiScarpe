96c8ac674515c956610a3ee2d2656d83972cb05c
kimkohlenbach <kim.kohlenbach@digitaldesk.com.br>
2018-05-08 14:49:56 -0300

    Função de selecionar numeração do calçado

--- DiScarpe/Scripts/script.js
+++ DiScarpe/Scripts/script.js
@@ -1,1 +1,6 @@
-﻿
+﻿$(document).ready(function () {
+    $('.nTamanho').click(function () {
+        $('.nTamanho.active').removeClass("active"); //aqui removemos a class do item anteriormente clicado para que possamos adicionar ao item clicado
+        $(this).addClass("active"); //aqui adicionamos a class ao item clicado
+    });
+});
